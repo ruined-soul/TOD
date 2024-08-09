df399a9558bb1d58d9c4aa076b2a1abdce9002fc
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-09 13:04:20 +0530

    Create __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import sessionmaker
+from models import engine, init_db
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+init_db()
