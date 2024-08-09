3a55b1e142219634ffe8b709dbaff4eb56e85776
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-09 12:28:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,46 @@
+# config.py
+
+# Bot Token
+BOT_TOKEN = "7291744111:AAECPDlB0WDC8bUC4MF9iGZ_z1_aSNL9nGk"
+
+# Bot Owners
+FIRST_BOT_OWNER_ID = 1159381624
+FIRST_BOT_OWNER_USERNAME = "ruined_soul"
+
+SECOND_BOT_OWNER_ID = 7329859398
+SECOND_BOT_OWNER_USERNAME = "indiphoenix"
+
+# Support Chat Details
+SUPPORT_CHAT_INVITE_LINK = "https://t.me/grouptdsupport"
+SUPPORT_CHAT_ID = -4282708596
+
+# Test Group Chat Details
+TEST_GROUP_CHAT_INVITE_LINK = "https://t.me/grouptdtest"
+TEST_GROUP_CHAT_ID = -1002214082592
+
+# Game Settings
+COIN_PRICE_SKIP_TOKEN = 10
+
+# Command Variables
+ALIVE_COMMAND = "/alive"
+START_COMMAND = "/start"
+HELP_COMMAND = "/help"
+FEEDBACK_COMMAND = "/feedback"
+ADD_TRUTH_COMMAND = "/addtruth"
+ADD_DARE_COMMAND = "/adddare"
+ADD_PENALTY_COMMAND = "/addpenalty"
+LIST_QUESTIONS_COMMAND = "/listquestions"
+REMOVE_QUESTION_COMMAND = "/removequestion"
+KICK_PLAYER_COMMAND = "/kplayer"
+BROADCAST_COMMAND = "/broadcast"
+STATS_COMMAND = "/stats"
+LISGC_COMMAND = "/lisgc"
+
+# Game Mode Options
+SOLO_MODE = "solo"
+MULTIPLAYER_MODE = "multiplayer"
+
+# Question Categories
+TRUTH_QUESTIONS = []
+DARE_QUESTIONS = []
+PENALTIES = []
