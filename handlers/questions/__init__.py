3cc42d38ae56c4f1c6301b74a05900a69b2102eb
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-09 12:47:43 +0530

    Create __init__.py

---
+++ handlers/questions/__init__.py
@@ -0,0 +1,13 @@
+from .truth import add_truth_question
+from .dare import add_dare_question
+from .add_question import add_question
+from .list_questions import list_questions
+from .remove_question import remove_question
+from .penalty import add_penalty_question
+from .add_penalty import add_penalty
+
+__all__ = [
+    "add_truth_question", "add_dare_question", "add_question",
+    "list_questions", "remove_question", "add_penalty_question",
+    "add_penalty"
+]
